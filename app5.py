68efb7043e1d6cbaa1d9d9c52f886f88210a47f7
gonia5555 <gonia555584@gmail.com>
2025-11-24 15:04:08 +0100

    secrets

--- app5.py
+++ app5.py
@@ -16,6 +16,13 @@ from openai import OpenAI
 QDRANT_COLLECTION_NAME = "aplikacja_do_znajdywania_znajomych"
 
 env = dotenv_values(".env")
+### Secrets using Streamlit Cloud Mechanism
+# https://docs.streamlit.io/deploy/streamlit-community-cloud/deploy-your-app/secrets-management
+if 'QDRANT_URL' in st.secrets:
+    env['QDRANT_URL'] = st.secrets['QDRANT_URL']
+if 'QDRANT_API_KEY' in st.secrets:
+    env['QDRANT_API_KEY'] = st.secrets['QDRANT_API_KEY']
+##
 
 
 EMBEDDING_DIM = 1536
